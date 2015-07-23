7f99cf23cfef2a2f6cc6a917a14bedc3264d3909
shadefinale <shadefinale@gmail.com>
2015-07-23 14:35:50 -0500

    Updated queries in authors and builds controllers.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,6 @@
 class AuthorsController < ApplicationController
   def index
-    @authors = Author.joins(:builds).group("builds.author_id").order("count(builds.author_id) desc")
+    @authors = Author.joins(:builds).group(:id).order("count(builds.author_id) desc")
   end
 
   def show
--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -1,12 +1,13 @@
 class BuildsController < ApplicationController
   def index
-    @skills = builds_by_skill(params[:char])
+    @skills = builds_by_skill(params[:char].to_i)
   end
 
 
   private
     def builds_by_skill(char=nil)
-      char.nil? ? Skill.includes(:builds).order("skills.name") : Skill.joins(:builds).includes(:builds).where('builds.char = ?', char.to_i).order("skills.name")
+      char = nil unless (1..7).include? char
+      char.nil? ? Skill.includes(:builds).order(:name) : Skill.joins(:builds).includes(:builds).where('builds.char = ?', char.to_i).order("skills.name")
     end
 
 end
