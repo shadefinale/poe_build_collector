bd5f8214332ca1465434fb933c04f1e0b782669b
shadefinale <shadefinale@gmail.com>
2015-10-04 16:29:47 -0500

    Include authors and skills in builds.

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -1,12 +1,12 @@
 class BuildsController < ApplicationController
   def index
     if params[:char] && (1..7).include?(params[:char].to_i)
-      @builds = Build.includes(:skill).where(char: params[:char].to_i)
+      @builds = Build.includes(:skill).includes(:author).where(char: params[:char].to_i)
     else
-      @builds = Build.includes(:skill)
+      @builds = Build.includes(:skill).includes(:author)
     end
     respond_to do |format|
-      format.json { render json: @builds }
+      format.json { render json: @builds.to_json(include: [:skill, :author]) }
     end
   end
 end
