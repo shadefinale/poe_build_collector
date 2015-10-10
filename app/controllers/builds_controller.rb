efe119e1d4ad7c6732627d7382112915d40f943d
shadefinale <shadefinale@gmail.com>
2015-10-10 13:25:36 -0500

    Look into bower for rails.

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -1,10 +1,6 @@
 class BuildsController < ApplicationController
   def index
-    if params[:char] && (1..7).include?(params[:char].to_i)
-      @builds = Build.includes(:skill).includes(:author).where(char: params[:char].to_i)
-    else
-      @builds = Build.includes(:skill).includes(:author)
-    end
+    @builds = Build.includes(:skill).includes(:author)
     respond_to do |format|
       format.json { render json: @builds.to_json(include: [:skill, :author]) }
     end
