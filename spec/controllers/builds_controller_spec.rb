9f9df056c5597d69dbe0754cf167a9fce0c29b8a
shadefinale <shadefinale@gmail.com>
2015-10-31 17:20:10 -0500

    Remove non-used test.

--- spec/controllers/builds_controller_spec.rb
+++ spec/controllers/builds_controller_spec.rb
@@ -18,9 +18,4 @@ RSpec.describe BuildsController, type: :controller do
     expect(assigns(:builds)).to match_array([build1, build2, build3,
                                              build4, build5, build6, build7])
   end
-
-  it 'should get only the first build for the first character' do
-    get :index, format: :json, char: "1"
-    expect(assigns(:builds)).to match_array([build1])
-  end
 end
